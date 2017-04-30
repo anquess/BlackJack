6eabca54129819cf556030ad08b7aac2a19980a6
anquess <anquess@gmail.com>
2017-04-30 15:40:31 +0900

    AllTestを追加

---
+++ tst/jp/ac/ascsys/blackJack/AllTests.java
@@ -0,0 +1,11 @@
+package jp.ac.ascsys.blackJack;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ CardTest.class, NumTest.class, SuitTest.class })
+public class AllTests {
+
+}
