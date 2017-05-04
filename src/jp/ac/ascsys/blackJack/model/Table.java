3f1aa76b7d16e05e99367dfdbbfcc97d94cb381e
anquess <anquess@gmail.com>
2017-05-05 07:46:13 +0900

    表示系インターフェースViewInterfaceを作成、Tableクラスに実装
    表示はViewInterfaceに委譲するため

--- src/jp/ac/ascsys/blackJack/model/Table.java
+++ src/jp/ac/ascsys/blackJack/model/Table.java
@@ -1,10 +1,35 @@
 package jp.ac.ascsys.blackJack.model;
 
+import jp.ac.ascsys.blackJack.view.ViewInterface;
+
 public class Table {
 	private Hand garbage;
+	private Dealer dealer;
+	private Player player;
+	private ViewInterface view;
 
 	public Table(){
 		this.garbage = new Hand();
+	}
+
+	public Dealer getDealer() {
+		return dealer;
+	}
+
+	public void setView(ViewInterface view) {
+		this.view = view;
+	}
+
+	public void setDealer(Dealer dealer) {
+		this.dealer = dealer;
+	}
+
+	public Player getPlayer() {
+		return player;
+	}
+
+	public void setPlayer(Player player) {
+		this.player = player;
 	}
 
 	public void throwAwayHandForGarbage(Character character){
@@ -13,5 +38,16 @@ public class Table {
 	}
 	public Hand getGarbage(){
 		return this.garbage;
+	}
+
+	public void showTable(){
+		view.showTable();
+	}
+	public void showMoney(){
+		view.showMoney();
+	}
+
+	public void showResult(){
+		view.showResult();
 	}
 }
